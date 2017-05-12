b6d8a8be99b00d71ff46e3639793e0800a5a47b7
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2017-05-12 14:30:46 +0200

    added some state stuff

---
+++ Bejeweled clone 0_2/States/AbstractState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Bejeweled_clone_0_2.Board;
+using Bejeweled_clone_0_2.Graphics;
+
+namespace Bejeweled_clone_0_2.States
+{
+    abstract class AbstractState : IState
+    {
+        protected IPlayBoard playBoard;
+        protected RelativeRenderTarget playboardRenderTarget;
+        protected StateManager stateManager;
+        public AbstractState(IPlayBoard playBoard, RelativeRenderTarget playboardRenderTarget, StateManager stateManager)
+        {
+            this.playBoard = playBoard;
+            this.playboardRenderTarget = playboardRenderTarget;
+            this.stateManager = stateManager;
+        }
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch, GraphicsDevice graphicsDevice);
+        public abstract void OnPush(GameTime gameTime);
+    }
+}
---
+++ Bejeweled clone 0_2/States/DroppingCalculationState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Bejeweled_clone_0_2.Board;
+using Bejeweled_clone_0_2.Graphics;
+
+namespace Bejeweled_clone_0_2.States
+{
+    class DroppingCalculationState : AbstractState
+    {
+        public DroppingCalculationState(IPlayBoard playBoard, RelativeRenderTarget playboardRenderTarget, StateManager stateManager) : base(playBoard, playboardRenderTarget, stateManager)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch, GraphicsDevice graphicsDevice)
+        {
+        }
+
+        public override void OnPush(GameTime gameTime)
+        {
+            var i = playBoard.DropCalculationUpdate();
+            if(i.Count() > 0)
+            {
+                //animation state
+            }
+            else
+            {
+                //clearing state
+            }
+        }
+    }
+}
